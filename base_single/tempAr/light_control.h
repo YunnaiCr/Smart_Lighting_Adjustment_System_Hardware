34da05a336cfc5c07e565e42b01fadf92863e68d
WyxBUPT-22 <wuyuxin55@bupt.edu.cn>
2025-06-10 17:06:04 +0800

    单房间无问题代码

---
+++ base_single/tempAr/light_control.h
@@ -0,0 +1,21 @@
+// light_control.h
+#ifndef LIGHT_CONTROL_H
+#define LIGHT_CONTROL_H
+
+#include <Arduino.h> // for analogWrite, Serial, constrain, map, max, min
+#include "config.h" // 包含配置信息 (如引脚定义, RGB常量)
+#include "state.h"  // 包含状态变量
+#include "mqtt_handler.h"
+void setLEDColor(int red, int green, int blue);
+void setSceneMode(const String& mode);
+void setBaseColorForManualMode(const String& colorName);
+void adjustBrightnessForManualMode(float level);
+void adjustLightingBasedOnSensor();
+void checkVoiceCommands();
+void setOperationMode(const String& newMode);
+void setManualModeState(bool targetPart, String targetSceneMode, int r, int g, int b);
+void handlePartChange(bool newPart);
+void updatePartyLights();
+void updateStrobeEffect();
+void hsvToRgb(int h, int s, int v, int &r, int &g, int &b);
+#endif
---
+++ base_single/tempAr/network.h
@@ -0,0 +1,19 @@
+// network.h
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <BearSSLHelpers.h> // for BearSSL::WiFiClientSecure
+#include "config.h" // 包含配置信息
+#include "state.h"  // 包含状态变量
+
+// 声明外部链接的MQTT客户端
+extern BearSSL::WiFiClientSecure espClient;
+extern PubSubClient mqtt_client;
+
+void connectToWiFi();
+void syncTime();
+bool connectToMQTTBroker();
+
+#endif
